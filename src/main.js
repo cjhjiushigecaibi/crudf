234915be07f02b66f0ca70778151fb39d2fad5a9
cjh <494112930@qq.com>
2021-07-04 09:51:45 +0800

    vue 实现登录的页面+功能 增删查改的基本界面

--- src/main.js
+++ src/main.js
@@ -1,6 +1,14 @@
 import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
+import './assets/css/global.css'//添加全局样式
+import './plugins/element.js'
+// 导入axios
+import axios from 'axios'
+// 挂载axios 到Vue的原型prototype的$http
+Vue.prototype.$http = axios
+// 设置请求的根路径,后台端口：8899
+axios.defaults.baseURL = "http://localhost:8899/"
 
 Vue.config.productionTip = false
 
--- src/router/index.js
+++ src/router/index.js
@@ -1,27 +1,60 @@
 import Vue from 'vue'
 import VueRouter from 'vue-router'
-import Home from '../views/Home.vue'
+import Login from '../components/Login.vue'//引入login组件
+import Homead from '../components/Homead.vue'//导入homead组件
+import Homeuser from '../components/Homeuser.vue'//导入homeuser组件
+import add from '../components/add.vue'//导入add组件
+import dele from '../components/dele.vue'//导入delete组件
+import searchad from '../components/searchad.vue'//导入search组件
+import searchuser from '../components/searchuser.vue'//导入search组件
+import update from '../components/update.vue'//导入update组件
 
 Vue.use(VueRouter)
 
 const routes = [
-  {
-    path: '/',
-    name: 'Home',
-    component: Home
-  },
-  {
-    path: '/about',
-    name: 'About',
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    component: () => import(/* webpackChunkName: "about" */ '../views/About.vue')
-  }
+	{
+		path:"/",
+		redirect:"/login"
+	},
+	{
+		path: '/login',
+		component: Login
+	},
+	{
+	  path: '/home1',
+	  component: Homead,
+	  children: [
+		  {
+	      path: '/add',
+	      component: add,
+			},
+	  	  {
+		  path: '/dele',
+	  	  component: dele,
+	  		},
+			
+			{
+			path: '/update',
+			component: update,
+			},
+			{
+			path: '/searchad',
+			component: searchad,
+			},]
+	},
+	{
+	  path: '/home0',
+	  component: Homeuser,
+	  children: [{
+	      path: '/searchuser',
+	      component: searchuser,
+		  }]  
+	},
 ]
 
 const router = new VueRouter({
   routes
 })
+
 
 export default router
